e4b85e519a05e8f8ba845901ceca220e5e96fbea
Zach Eriksen <zmeriksen@icloud.com>
2022-04-07 19:50:33 -0600

    Update SDMarkdown

--- Sources/SD2MD/Markdown/SomeView+Markdown.swift
+++ Sources/SD2MD/Markdown/SomeView+Markdown.swift
@@ -15,46 +15,6 @@ enum CodeType: String {
 }
 
 extension SomeView {
-    static func vstack(_ views: [SomeView]) -> Self {
-        SomeContainerView(views: views).someView
-    }
-    
-    static func image(
-        url: String,
-        width: Int? = nil,
-        height: Int? = nil,
-        destination: Destination? = nil
-    ) -> Self {
-        SomeImage(
-            url: url,
-            width: width,
-            height: height,
-            destination: destination
-        ).someView
-    }
-    
-    static func label(
-        title: String,
-        subtitle: String? = nil,
-        font: FontType = .caption,
-        destination: Destination? = nil
-    ) -> Self {
-        SomeLabel(
-            title: title,
-            subtitle: subtitle,
-            font: font,
-            destination: destination
-        ).someView
-    }
-    
-    static func text(_ text: String) -> Self {
-        SomeText(title: text).someView
-    }
-    
-    static func spacer(size: Int = 1) -> Self {
-        SomeSpacer(size: size * 8).someView
-    }
-    
     static func code(
         type: CodeType,
         _ block: () -> String
